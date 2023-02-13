ab5178415c7781d5a918970911b0cc6aec1541ab
Athiriyya <athiriyya@gmail.com>
2023-02-13 14:14:08 -0600

    TxReceipt & address are actually defined in the web3 package; take our types from there

--- abi_maker/template_modules/solidity_types.py
+++ abi_maker/template_modules/solidity_types.py
@@ -1,13 +1,14 @@
 
 from typing import Sequence, Dict, Tuple
 from web3.datastructures import AttributeDict
+from web3.types import TxReceipt, ChecksumAddress
 
 # We lose some detail here; Python doesnt have signed/unsigned int differentions
-# or bytes sizes. For now, these type aliase let us use Soliditys native types 
+# or bytes sizes. For now, these type aliase let us use Solidity's native types 
 # as type hints while being a little less particular to Pythons Mypy type analyzer
-address = str
+address = ChecksumAddress
+HexAddress = ChecksumAddress
 bool = bool
-# tuple = Sequence
 string = str
 uint8 = int
 uint16 = int
@@ -17,8 +18,9 @@ uint128 = int
 uint256 = int
 int64 = int
 int256 = int
-# bytes = bytes
 bytes4 = bytes
 bytes32 = bytes
-
-TxReceipt = AttributeDict 
+# These show up in some ABIs, but there's some extra work we'd need to do
+# to use them in Python since they're already reserved words
+# bytes = bytes
+# tuple = Sequence
